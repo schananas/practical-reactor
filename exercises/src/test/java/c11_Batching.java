525bc5288684ee00f57ddc8028eda70b9b3ab23a
Stefan <91stefan@gmail.com>
2022-03-26 09:11:25 +0100

    solutions c11_Batching

--- exercises/src/test/java/c11_Batching.java
+++ exercises/src/test/java/c11_Batching.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.*;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 /**
  * Another way of controlling amount of data flowing is batching.
  * Reactor provides three batching strategies: grouping, windowing, and buffering.
@@ -26,10 +28,10 @@ public class c11_Batching extends BatchingBase {
      */
     @Test
     public void batch_writer() {
-        //todo do your changes here
-        Flux<Void> dataStream = null;
-        dataStream();
-        writeToDisk(null);
+        Flux<Void> dataStream = dataStream()
+                .buffer(10)
+                .flatMap(this::writeToDisk);
+
 
         //do not change the code below
         StepVerifier.create(dataStream)
@@ -47,10 +49,11 @@ public class c11_Batching extends BatchingBase {
      */
     @Test
     public void command_gateway() {
-        //todo: implement your changes here
-        Flux<Void> processCommands = null;
-        inputCommandStream();
-        sendCommand(null);
+        Flux<Void> processCommands = inputCommandStream()
+                .groupBy(Command::getAggregateId)
+                .flatMap(aggregateCommands -> //execute commands from same aggregate in parallel
+                                 aggregateCommands.concatMap(this::sendCommand) //execute commands from same aggregate sequentially
+                );
 
         //do not change the code below
         StepVerifier.create(processCommands)
@@ -65,7 +68,10 @@ public class c11_Batching extends BatchingBase {
     @Test
     public void sum_over_time() {
         Flux<Long> metrics = metrics()
-                //todo: implement your changes here
+                .window(Duration.ofSeconds(1))
+                .concatMap(window -> window.reduce(0L, Long::sum)).doOnNext(sum -> System.out.println(
+                        "sum last second: " +
+                                sum))
                 .take(10);
 
         StepVerifier.create(metrics)
