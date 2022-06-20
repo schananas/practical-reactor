605d041b7b6a19206a952ad69ee2212ed89b3f25
danpeczek <danpeczek@gmail.com>
2022-06-20 21:04:21 +0200

    batching with solutions

--- exercises/src/test/java/c11_Batching.java
+++ exercises/src/test/java/c11_Batching.java
@@ -1,18 +1,21 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 /**
  * Another way of controlling amount of data flowing is batching.
  * Reactor provides three batching strategies: grouping, windowing, and buffering.
- *
+ * <p>
  * Read first:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#advanced-three-sorts-batching
  * https://projectreactor.io/docs/core/release/reference/#which.window
- *
+ * <p>
  * Useful documentation:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#which-operator
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html
@@ -27,13 +30,13 @@ public class c11_Batching extends BatchingBase {
     @Test
     public void batch_writer() {
         //todo do your changes here
-        Flux<Void> dataStream = null;
-        dataStream();
-        writeToDisk(null);
+        Flux<Void> dataStream = dataStream()
+                .buffer(10)
+                .flatMap(this::writeToDisk);
 
         //do not change the code below
         StepVerifier.create(dataStream)
-                    .verifyComplete();
+                .verifyComplete();
 
         Assertions.assertEquals(10, diskCounter.get());
     }
@@ -47,14 +50,14 @@ public class c11_Batching extends BatchingBase {
      */
     @Test
     public void command_gateway() {
-        //todo: implement your changes here
-        Flux<Void> processCommands = null;
-        inputCommandStream();
-        sendCommand(null);
+
+        Flux<Void> processCommands = inputCommandStream()
+                .groupBy(Command::getAggregateId)
+                .flatMap(aggregateCommands -> aggregateCommands.concatMap(this::sendCommand));
 
         //do not change the code below
         StepVerifier.create(processCommands)
-                    .verifyComplete();
+                .verifyComplete();
     }
 
 
@@ -63,13 +66,16 @@ public class c11_Batching extends BatchingBase {
      * metric readings that have been published during one second.
      */
     @Test
+    //Todo: flaky
     public void sum_over_time() {
         Flux<Long> metrics = metrics()
-                //todo: implement your changes here
+                .window(Duration.ofSeconds(1))
+                .concatMap(window -> window.reduce(0L, Long::sum))
+                .doOnNext(sum -> System.out.println("sum last second: " + sum))
                 .take(10);
 
         StepVerifier.create(metrics)
-                    .expectNext(45L, 165L, 255L, 396L, 465L, 627L, 675L, 858L, 885L, 1089L)
-                    .verifyComplete();
+                .expectNext(45L, 165L, 255L, 396L, 465L, 627L, 675L, 858L, 885L, 1089L)
+                .verifyComplete();
     }
 }
