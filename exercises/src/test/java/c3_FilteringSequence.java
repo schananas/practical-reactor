e748bc49abac486002935d723f2712b18b50f315
Lucas <lucasalexanderhenry@gmail.com>
2022-12-30 17:12:05 -0800

    Update c3_FilteringSequence.java
    Fix minor bug in needle_in_a_haystack test. Object variable type should be changed to String so the ofType solution.

--- exercises/src/test/java/c3_FilteringSequence.java
+++ exercises/src/test/java/c3_FilteringSequence.java
@@ -41,7 +41,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
      */
     @Test
     public void needle_in_a_haystack() {
-        Flux<Object> strings = mashed_data_service()
+        Flux<String> strings = mashed_data_service()
                 //todo: change this line only
                 ;
 
